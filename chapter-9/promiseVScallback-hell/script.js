8b60af02ee44e111ee4eb25dd92a3676a122cb13
Ashish Jain <ashishjain99@yahoo.com>
2024-09-12 19:23:25 -0600

    Understood how to chain promises

--- chapter-9/promiseVScallback-hell/script.js
+++ chapter-9/promiseVScallback-hell/script.js
@@ -1,3 +1,4 @@
+
 function getData(endpoint)
 {
     return new Promise((resolve, reject) => 
@@ -8,9 +9,16 @@ function getData(endpoint)
 
         xhr.onreadystatechange = function()
         {
-            if(this.readyState === 4 && this.status === 200)
+            if(this.readyState === 4)
             {
-                console.log(JSON.parse(this.responseText));
+                if(this.status === 200)
+                {
+                    resolve(JSON.parse(this.responseText));
+                }
+                else
+                {
+                    reject("Something Went Wrong");
+                }
             };
         };
         setTimeout(() => 
@@ -19,3 +27,18 @@ function getData(endpoint)
             }, Math.floor(Math.random() * 3000) + 1000);
     });
 };
+getData('./movies.json')
+    .then((movies) => 
+        {
+        console.log(movies); 
+        return getData('./actors.json');
+    })
+    .then((actors) => 
+    {
+        console.log(actors);
+        return getData('./directors.json');
+    })
+    .then((directors) => 
+        {
+            console.log(directors);
+        })
