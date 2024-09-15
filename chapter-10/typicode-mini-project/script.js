92ca81848b6226a524cbfd9eca0e6fb1354b9230
Ashish Jain <ashishjain99@yahoo.com>
2024-09-15 15:23:46 -0600

    added typicode

--- chapter-10/typicode-mini-project/script.js
+++ chapter-10/typicode-mini-project/script.js
@@ -1,26 +1,83 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
-const getTodos = () => 
-    {
-        fetch(apiUrl + '?_limit=5')
-        .then((response) => response.json())
-        .then((data) => 
-            {
-                data.forEach((todo) => 
-                    {
-                        const div = document.createElement('div');
-                        div.classList = 'border-2 px-4 py-2 border-black'
-                        div.appendChild(document.createTextNode(todo.title));
-                        div.setAttribute('data-id',todo.id);
-                        if(todo.completed)
-                            {
-                                div.classList.add('bg-gray-400');
-                            }
+const getTodos = () => {
+    fetch(apiUrl + '?_limit=10')
+        .then((res) => res.json())
+        .then((data) => {
+            data.forEach((todo) => addTodoToDOM(todo));
+        });
+};
 
-                        document.getElementById('todo-list').appendChild(div);
-                    })
-            })
-        .catch((error) => console.log(error))
+const addTodoToDOM = (todo) => {
+    const div = document.createElement('div');
+    div.classList = 'mt-4 border-2 px-4 py-2 border-black';
+    div.classList.add('todo');
+    div.appendChild(document.createTextNode(todo.title));
+    div.setAttribute('data-id', todo.id);
+
+    if (todo.completed) {
+        div.classList.add('done');
+        div.classList.add('bg-gray-300')
+    }
+
+    document.getElementById('todo-list').appendChild(div);
+};
+
+const createTodo = (e) => {
+    e.preventDefault();
+
+    const newTodo = {
+        title: e.target.firstElementChild.value,
+        completed: false,
     };
 
-getTodos();
+    fetch(apiUrl, {
+        method: 'POST',
+        body: JSON.stringify(newTodo),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+        .then((res) => res.json())
+        .then((data) => addTodoToDOM(data));
+};
+
+const toggleCompleted = (e) => {
+    if (e.target.classList.contains('todo')) {
+        e.target.classList.toggle('done');
+        e.target.classList.toggle('bg-gray-300');
+        updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+    }
+};
+
+const updateTodo = (id, completed) => {
+    fetch(`${apiUrl}/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ completed }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+};
+
+const deleteTodo = (e) => {
+    if (e.target.classList.contains('todo')) {
+        const id = e.target.dataset.id;
+        fetch(`${apiUrl}/${id}`, {
+            method: 'DELETE',
+        })
+            .then((res) => res.json())
+            .then(() => e.target.remove());
+    }
+};
+
+const init = () => {
+    document.addEventListener('DOMContentLoaded', getTodos);
+    document.querySelector('#todo-form').addEventListener('submit', createTodo);
+    document
+        .querySelector('#todo-list')
+        .addEventListener('click', toggleCompleted);
+    document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
+};
+
+init();
