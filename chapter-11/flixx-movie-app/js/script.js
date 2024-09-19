7c20638c3ba6eebaf1540389b81f659d0e39922d
Ashish Jain <ashishjain99@yahoo.com>
2024-09-19 14:33:44 -0600

    Backdrop image added

--- chapter-11/flixx-movie-app/js/script.js
+++ chapter-11/flixx-movie-app/js/script.js
@@ -49,7 +49,10 @@ async function displayPopularShows() {
 async function displayMovieDetails() {
     const movieId = window.location.search.split('=')[1];
     const movie = await fetchAPIData(`movie/${movieId}`);
-console.log(movie.poster_path)
+
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
+    
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="details-top">
@@ -88,6 +91,33 @@ console.log(movie.poster_path)
         </div>
     `
     document.querySelector('#movie-details').appendChild(div);
+};
+
+
+// Display backdrop on details page
+function displayBackgroundImage(type, backgroundPath)
+{
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center';
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if(type === 'movie')
+    {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    }
+    else
+    {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
 }
 
 // Fetch data from TMDB API
