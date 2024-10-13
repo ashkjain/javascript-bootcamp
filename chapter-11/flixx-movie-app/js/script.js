15a0d2b037a845b32c3db4e23600ef5e0803e7c9
Ashish Jain <ashishjain99@yahoo.com>
2024-10-13 15:21:20 -0600

    Added TV show display app

--- chapter-11/flixx-movie-app/js/script.js
+++ chapter-11/flixx-movie-app/js/script.js
@@ -31,7 +31,7 @@ async function displayPopularShows() {
         const div = document.createElement('div');
         div.classList.add('card');
         div.innerHTML = `
-            <a href="movie-details.html?id=${show.id}">
+            <a href="tv -details.html?id=${show.id}">
                     ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.name}" />` : `<img src="images/no-image.jpg" class="card-img-top" alt="${show.name}" />`
             }
                 </a>
@@ -52,7 +52,7 @@ async function displayMovieDetails() {
 
     // Overlay for background image
     displayBackgroundImage('movie', movie.backdrop_path);
-    
+
     const div = document.createElement('div');
     div.innerHTML = `
     <div class="details-top">
@@ -93,10 +93,57 @@ async function displayMovieDetails() {
     document.querySelector('#movie-details').appendChild(div);
 };
 
+// Display Show Details
+async function displayShowDetails() {
+    const showId = window.location.search.split('=')[1];
+    const show = await fetchAPIData(`tv/${showId}`);
+
+    // Overlay for background image
+    displayBackgroundImage('tv', show.backdrop_path);
+
+    const div = document.createElement('div');
+    div.innerHTML = `
+    <div class="details-top">
+          <div>
+            ${show.poster_path ? `<img src="https://image.tmdb.org/t/p/w500${show.poster_path}" class="card-img-top" alt="${show.title}"` : `<img src="../images/no-image.jpg" class="card-img-top" alt="${show.title}"`}
+          </div>
+          <div>
+            <h2>${show.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${show.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${show.release_date}</p>
+            <p>
+              ${show.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${show.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $${addCommasToNumber(show.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $${addCommasToNumber(show.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${show.runtime} minutes</li>
+            <li><span class="text-secondary">Status:</span> ${show.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">
+          ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+          </div>
+        </div>
+    `
+    document.querySelector('#show-details').appendChild(div);
+};
+
 
 // Display backdrop on details page
-function displayBackgroundImage(type, backgroundPath)
-{
+function displayBackgroundImage(type, backgroundPath) {
     const overlayDiv = document.createElement('div');
     overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
     overlayDiv.style.backgroundSize = 'cover';
@@ -110,12 +157,10 @@ function displayBackgroundImage(type, backgroundPath)
     overlayDiv.style.zIndex = '-1';
     overlayDiv.style.opacity = '0.1';
 
-    if(type === 'movie')
-    {
+    if (type === 'movie') {
         document.querySelector('#movie-details').appendChild(overlayDiv);
     }
-    else
-    {
+    else {
         document.querySelector('#show-details').appendChild(overlayDiv);
     }
 }
@@ -151,9 +196,8 @@ function highlightActiveLink() {
 };
 
 // Add commas to numbers
-function addCommasToNumber(number)
-{
-    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g,',');
+function addCommasToNumber(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
 
 // Init App
@@ -170,7 +214,7 @@ function init() {
             displayMovieDetails();
             break;
         case '/tv-details.html':
-            console.log('TV Details');
+            displayShowDetails();
             break;
         case '/search.html':
             console.log('Search')
