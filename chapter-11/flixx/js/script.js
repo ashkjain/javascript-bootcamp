93ca23fa35ae4918ee5b965adb21671392cb5f69
Ashish Jain <ashishjain99@yahoo.com>
2024-10-20 23:04:31 -0600

    Added Diplay popular movies function with fetch function

--- chapter-11/flixx/js/script.js
+++ chapter-11/flixx/js/script.js
@@ -1,37 +1,76 @@
 // Global Variables
-const global = 
+const global =
 {
     currentPage: window.location.pathname
 }
 
 // Global Variables
 
+// Fetch data from the TMDB API
+// Fetch Function
+async function fetchAPIData(endpoint) {
+    const API_KEY = '96a295ed5e51ddb6255b1ba01f334994';
+    const API_URL = 'https://api.themoviedb.org/3/';
+
+    const response = await fetch(`${API_URL}${endpoint}?api_key=${API_KEY}&language=en-US`);
+    const data = await response.json();
+    return data;
+}
+// Fetch Function
+
+// Function to receive Popular Movies using FetchAPIData function (Will be used on Main Page)
+// Display Popular Movies
+async function displayPopularMovies() {
+    const { results } = await fetchAPIData('movie/popular');
+    results.forEach(movie => {
+        const div = document.createElement('div');
+        div.classList.add('card');
+        div.innerHTML = `<div class="card">
+          <a href="movie-details.html?id=${movie.id}">
+            ${movie.poster_path ? `<img
+              src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+              class="card-img-top"
+              alt="${movie.title}"
+            />` : `<img
+              src="images/no-image.jpg"
+              class="card-img-top"
+              alt="${movie.title}"
+            />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${movie.title}</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${movie.release_date}</small>
+            </p>
+          </div>
+        </div>`
+        document.querySelector('#popular-movies').appendChild(div);
+    })
+};
+// Display Popular Movies
+
 
 // Higlight active link function
 // Highlight
-function higlightActiveLink()
-{
+function higlightActiveLink() {
     const links = document.querySelectorAll('.nav-link');
-    links.forEach((link) => 
-        {
-            if(link.getAttribute('href') === global.currentPage)
-            {
-                link.classList.add('active');
-            }
-        })
+    links.forEach((link) => {
+        if (link.getAttribute('href') === global.currentPage) {
+            link.classList.add('active');
+        }
+    })
 };
 // Highlight
 
 
 // Router for pages from here
 // Router
-function init()
-{
-    switch(global.currentPage)
-    {
+function init() {
+    switch (global.currentPage) {
         case '/':
         case '/index.html':
-            console.log("Home");
+            displayPopularMovies();
             break;
         case '/shows.html':
             console.log("Shows");
