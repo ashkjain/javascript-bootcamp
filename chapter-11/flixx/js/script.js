6a193e39a81f3e87d05ec917a4b33b3a0b55275e
Ashish Jain <ashishjain99@yahoo.com>
2024-10-23 22:34:54 -0600

    Details for individual movie function added

--- chapter-11/flixx/js/script.js
+++ chapter-11/flixx/js/script.js
@@ -67,6 +67,69 @@ async function displayPopularMovies() {
 };
 // Display Popular Movies
 
+// This function will show the movie details which is available on the other page by redirecting
+// Display Movie Details
+async function displayMovieDetails()
+{
+  const movieId = window.location.search.split('=')[1];
+  const movie = await fetchAPIData(`movie/${movieId}`);
+
+  const div = document.createElement('div');
+  div.innerHTML = `
+  <div class="details-top">
+          <div>
+          ${movie.poster_path ? `<img
+          src="https://image.tmdb.org/t/p/w500${movie.poster_path}"
+          class="card-img-top"
+          alt="${movie.title}"
+        />` : `<img
+        src="images/no-image.jpg"
+        class="card-img-top"
+        alt="${movie.title}"
+      />`}
+            
+          </div>
+          <div>
+            <h2>${movie.title}</h2>
+            <p>
+              <i class="fas fa-star text-primary"></i>
+              ${movie.vote_average.toFixed(1)} / 10
+            </p>
+            <p class="text-muted">Release Date: ${movie.release_date}</p>
+            <p>
+              ${movie.overview}
+            </p>
+            <h5>Genres</h5>
+            <ul class="list-group">
+              ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+            </ul>
+            <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
+          </div>
+        </div>
+        <div class="details-bottom">
+          <h2>Movie Info</h2>
+          <ul>
+            <li><span class="text-secondary">Budget:</span> $ ${addCommasToNumber(movie.budget)}</li>
+            <li><span class="text-secondary">Revenue:</span> $ ${addCommasToNumber(movie.revenue)}</li>
+            <li><span class="text-secondary">Runtime:</span> ${movie.runtime} Minutes</li>
+            <li><span class="text-secondary">Status:</span> ${movie.status}</li>
+          </ul>
+          <h4>Production Companies</h4>
+          <div class="list-group">${movie.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}</div>
+        </div>`;
+
+        document.querySelector('#movie-details').appendChild(div);
+}
+// Display Movie Details
+
+// This function will add commas to the number that are shown in the details page for budget and revenue
+// Add Commas to Number
+function addCommasToNumber(number)
+{
+  return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+}
+// Add Commas to Number
+
 
 // Function to receive Popular Tv Shows using FetchAPIData function (Will be used on Main Page)
 // Display Popular TV Shows
@@ -127,7 +190,7 @@ function init() {
             console.log("Shows");
             break;
         case '/movie-details.html':
-            console.log("Movie Details");
+            displayMovieDetails();
             break;
         case '/tv-details.html':
             console.log("Tv Details");
